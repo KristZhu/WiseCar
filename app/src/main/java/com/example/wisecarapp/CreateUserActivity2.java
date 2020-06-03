8fdbbc6ab19ede4f4c7bd59f76233e421b2ab6a2
Krist-ZHU <krist.zhu@wisecar.co>
2020-06-03 15:44:13 +0930

    details modify

--- app/src/main/java/com/example/wisecarapp/CreateUserActivity2.java
+++ app/src/main/java/com/example/wisecarapp/CreateUserActivity2.java
@@ -192,12 +192,24 @@ public class CreateUserActivity2 extends AppCompatActivity {
 
     public boolean dispatchTouchEvent(MotionEvent ev) {
         View v = getCurrentFocus();
-        if (HideKeyBoard.isShouldHideInput(v, ev)) {
+        if (isShouldHideInput(v, ev)) {
             hideSoftInput(v.getWindowToken());
         }
         return super.dispatchTouchEvent(ev);
     }
-
+    private boolean isShouldHideInput(View v, MotionEvent event) {
+        if(v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0],
+                    top = l[1],
+                    bottom = top + v.getHeight(),
+                    right = left + v.getWidth();
+            return !(event.getX()>left && event.getX()<right
+                    && event.getY()>top && event.getY()<bottom);
+        }
+        return false;
+    }
     private void hideSoftInput(IBinder token) {
         if (token != null) {
             InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
