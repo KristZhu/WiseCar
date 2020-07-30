aa18ebbcc232b24a9ebc16a26c89ee429396d403
KristZHU <krist.zhu@wisecar.co>
2020-07-30 11:33:18 +1000

    AboutActivity

---
+++ app/src/main/java/com/wisecarCompany/wisecarapp/user/profile/AboutActivity.java
@@ -0,0 +1,32 @@
+package com.wisecarCompany.wisecarapp.user.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.wisecarCompany.wisecarapp.R;
+import com.wisecarCompany.wisecarapp.user.vehicle.VehicleActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    ImageButton backImageButton;
+    TextView linkTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        backImageButton = (ImageButton) findViewById(R.id.backImageButton);
+        backImageButton.setOnClickListener(v -> startActivity(new Intent(this, VehicleActivity.class)));
+
+        linkTextView = (TextView) findViewById(R.id.linkTextView);
+        linkTextView.setOnClickListener(v -> {
+
+        });
+
+    }
+}
